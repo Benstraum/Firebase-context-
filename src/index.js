cb1e783ee422219f9f1f16dc324bfe37cfdee58a
ben straumann <benstraum@gmail.com>
2020-10-25 16:09:38 -0500

    initial commit with base app and blank server/reduxstate

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './redux/reducers'; // imports ./redux/reducers/index.js
+// const sagaMiddleware = createSagaMiddleware();
+
+
+// const middlewareList = process.env.NODE_ENV === 'development' ?
+//   [sagaMiddleware] :
+//   [sagaMiddleware];
+
+
+const store = createStore(
+  // tells the saga middleware to use the rootReducer
+  // rootSaga contains all of our other reducers
+   rootReducer
+  // // adds all middleware to our project including saga and logger
+  // applyMiddleware(...middlewareList),
+ 
+);
+
+
 ReactDOM.render(
-  <React.StrictMode>
+ 
+    <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
